369a2d4699f1444df397419256025ad6727bbb23
seanwieser <seanwieser@gmail.com>
2023-04-09 23:18:17 -0700

    big changes

---
+++ fantasy-footballer-webapp/src/pages/home/home.js
@@ -0,0 +1,12 @@
+import './home.css';
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            Home page           
+        </div>
+    )
+}
+
+export default Home;

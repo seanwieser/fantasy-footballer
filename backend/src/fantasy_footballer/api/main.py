1503a5ca0d66933b528fb73579f2bf323802eb71
seanwieser <seanwieser@gmail.com>
2023-06-19 23:49:39 -0700

    Backend stuff.

--- backend/src/fantasy_footballer/api/main.py
+++ backend/src/fantasy_footballer/api/main.py
@@ -1,9 +1,9 @@
 """Module that contains the backend API."""
 
-from api import crud, models, schemas
-from api.database import SessionLocal, engine
-from api.models import Team
-from api.schemas import Response, TeamSchema
+from fantasy_footballer.api import crud, models, schemas
+from fantasy_footballer.api.database import SessionLocal, engine
+from fantasy_footballer.api.models import Team
+from fantasy_footballer.api.schemas import Response, TeamSchema
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
@@ -19,37 +19,29 @@ def get_db():
         db.close()
 
 
-# @app.get("/")
-# def read_root(db: Session = Depends(get_db)):
-#     """Root path"""
-#     """Root of API."""
-#     return crud.get_team_by_name(db=db, name='cmag')
-
-
-@app.get("/teams/")
-def read_teams(skip: int = 0, limit: int = 0, db: Session = Depends(get_db)):
+@app.get("/teams/", response_model=list[TeamSchema])
+def read_teams(db: Session = Depends(get_db)):
     """Get list of all teams."""
-    teams = crud.get_teams(db, skip=skip, limit=limit)
+    teams = crud.get_teams(db)
     return teams
 
 
-@app.get("/teams/{name}/")
+@app.get("/teams/name/{name}/", response_model=TeamSchema)
 def read_team(name: str, db: Session = Depends(get_db)):
     """Get team by name endpoint."""
     team = crud.get_team_by_name(db=db, name=name)
     return team
 
 
-@app.post("/teams/")
-def create_team(team: dict, db: Session = Depends(get_db)):
-    """Create team post."""
-    # db_team = crud.get_team_by_id(db, id=team.team_id)
-    # if db_team:
-    #     raise HTTPException(status_code=400, detail="Team already registered")
-    return crud.create_team(db=db, team=team)
+@app.get("/teams/year/{year}/", response_model=list[TeamSchema])
+def read_team_by_year(year: str, db: Session = Depends(get_db)):
+    """Get team data for a particular year."""
+    teams = crud.get_teams_by_year(db=db, year=year)
+    return teams
 
 
-# @app.get("/members/{year}")
-# async def read_page(year: int) -> list[str]:
-#     """Path parameter example."""
-#     return fetch_members(year)
+@app.get("/teams/leaderboard/{year}/")
+def read_leaderboard_by_year(year: str, db: Session = Depends(get_db)):
+    """Get ranking of owners for a particular year."""
+    teams = crud.get_leaderboard_by_year(db=db, year=year)
+    return teams

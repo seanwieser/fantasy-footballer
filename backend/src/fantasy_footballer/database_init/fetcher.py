419947c08c1939e3c84e46a3599a7c4500cc06ce
seanwieser <seanwieser@gmail.com>
2023-07-11 12:34:12 -0700

    Ingestion and backend for players.

--- backend/src/fantasy_footballer/database_init/fetcher.py
+++ backend/src/fantasy_footballer/database_init/fetcher.py
@@ -18,20 +18,20 @@ DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_NAME = os.getenv('DB_NAME')
 
 
-def extract_teams(year: int) -> list[Team]:
+def extract_year(year: int) -> list[Team]:
     """Return team object from pickle file."""
     # league = League(league_id=LEAGUE_ID, year=year, espn_s2=ESPN_S2, swid=SWID)
-    with open(f'data/league_{year}.pkl', 'rb') as inp:
+    with open(f'../../../league_{year}.pkl', 'rb') as inp:
         league: League = pickle.load(inp)
-    return league.teams
+    return league
 
 
-def transform_teams(raw_teams: list[Team], year: int) -> list[dict]:
+def transform_teams(raw_teams: list[Team], year: str) -> list[dict]:
     """Team ingestion tranformations."""
     new_teams = []
     for team in raw_teams:
         team = team.__dict__
-        team['year'] = year
+        team["year"] = year
         team['roster'] = [
             player.__dict__['playerId'] for player in team['roster']
         ]
@@ -59,17 +59,66 @@ def transform_teams(raw_teams: list[Team], year: int) -> list[dict]:
     return new_teams
 
 
-def etl_team_data(year: int) -> list[dict]:
-    """Ingest API Team data to database Team schema."""
-    teams = extract_teams(year)
-    return transform_teams(teams, year)
+def extract_transform_players(league: League, year: str) -> list[dict]:
+    """Player ingestion extraction and transformations."""
+    player_stat_fields = [
+        'teamWin', 'receivingReceptions', 'receivingYards', 'receivingTargets',
+        'receivingTouchdowns', 'rushingAttempts', 'rushingYards',
+        'rushingTouchdowns', 'receiving2PtConversions',
+        'rushing2PtConversions', 'passingAttempts', 'passingCompletions',
+        'passingIncompletions', 'passingYards', 'passingTouchdowns',
+        'passingCompletionPercentage', 'passingTimesSacked', 'fumbles',
+        'lostFumbles', 'turnovers', 'madeFieldGoalsFrom40To49',
+        'attemptedFieldGoalsFrom40To49', 'madeFieldGoalsFromUnder40',
+        'attemptedFieldGoalsFromUnder40', 'madeFieldGoals',
+        'attemptedFieldGoals', 'madeExtraPoints', 'attemptedExtraPoints',
+        'pointsScored'
+    ]
+
+    players = []
+    for pick in league.draft:
+        pick = pick.__dict__
+
+        player = league.player_info(playerId=pick["playerId"])
+        player = player.__dict__
+        player["year"] = year
+        for week in player["stats"].keys():
+            player['stats'][week] = {
+                k: v
+                for k, v in player['stats'][week].items()
+                if k in ['breakdown', 'points']
+            }
+            player['stats'][week]['breakdown'] = {
+                k: v
+                for k, v in player['stats'][week]['breakdown'].items()
+                if k in player_stat_fields
+            }
+        pick = {
+            k: v
+            for k, v in pick.items()
+            if k in ["round_num", "round_pick", "bid_amount"]
+        }
+        players.append(player | pick)
+
+        print(player["name"])
+    return players
+
+
+def write_data(data: list[dict], kind: str, year: str) -> None:
+    """Write jsonl format to specific location dependning on kind."""
+    with open(f'data/{kind}/{kind}_{year}.json.csv', 'w',
+              encoding='utf-8') as out_file:
+        for line in data:
+            json.dump(line, out_file)
+            out_file.write('\n')
 
 
 if __name__ == '__main__':
     for file_year in [2018, 2019, 2020, 2021, 2022, 2023]:
-        data = etl_team_data(file_year)
-        with open(f'data/league_{file_year}.json.csv', 'w',
-                  encoding='utf-8') as f:
-            for line in data:
-                json.dump(line, f)
-                f.write('\n')
+        lg = extract_year(file_year)
+
+        transformed_teams = transform_teams(lg.teams, file_year)
+        write_data(transformed_teams, 'teams', file_year)
+
+        transformed_players = extract_transform_players(lg, file_year)
+        write_data(transformed_players, 'players', file_year)

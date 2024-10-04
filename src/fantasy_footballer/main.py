86a86a0cc9e745d8ba313352d4aef0ed281d8a62
Sean Wieser <seanwieser@gmail.com>
2024-10-04 12:14:57 -0700

    Pre-commit.

--- src/fantasy_footballer/main.py
+++ src/fantasy_footballer/main.py
@@ -1,11 +1,13 @@
 """Main entry point for the application."""
 
+# pylint: disable=reimported
+
 from backend.db_init import DbInit
 from backend.io_utils import MEDIA_PATH_TEMPLATE
-from frontend.home.page import home_page
-from frontend.leaderboard.page import leaderboard_page
-from frontend.owners.page import owner_page, owners_page
-from frontend.players.page import players_page
+from frontend.owners.home import page
+from frontend.owners.spotlight import page
+from frontend.players.page import page
+from frontend.splash.home import page
 from nicegui import app, ui
 
 app.on_startup(DbInit.init_tables)
@@ -13,5 +15,3 @@ ui.run(title="Sco Chos",
        favicon=MEDIA_PATH_TEMPLATE.substitute(sub_path="favicons/football",
                                               file_name="favicon.ico"),
        dark=None)
-
-

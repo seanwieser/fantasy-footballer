b43cfcd003df65cfaf7b1889f66b7f8efb283ff9
Sean Wieser <seanwieser@gmail.com>
2024-10-04 12:14:41 -0700

    Formatting and print statements.

--- src/fantasy_footballer/backend/engine.py
+++ src/fantasy_footballer/backend/engine.py
@@ -13,11 +13,11 @@ SQLALCHEMY_DATABASE_URL = Template(
 
 ASYNC_URI = SQLALCHEMY_DATABASE_URL.substitute(
     driver="postgresql+asyncpg",
-    username=os.environ['DB_USER'],
-    password=os.environ['DB_PASSWORD'],
-    host=os.environ['DB_HOST'],
+    username=os.environ["DB_USER"],
+    password=os.environ["DB_PASSWORD"],
+    host=os.environ["DB_HOST"],
     port="5432",
-    db=os.environ['DB_NAME'])
+    db=os.environ["DB_NAME"])
 async_engine = create_async_engine(ASYNC_URI, pool_pre_ping=True)
 async_session = async_sessionmaker(bind=async_engine,
                                    autoflush=False,
@@ -25,10 +25,10 @@ async_session = async_sessionmaker(bind=async_engine,
                                    expire_on_commit=False)
 
 URI = SQLALCHEMY_DATABASE_URL.substitute(driver="postgresql",
-                                         username=os.environ['DB_USER'],
-                                         password=os.environ['DB_PASSWORD'],
-                                         host=os.environ['DB_HOST'],
+                                         username=os.environ["DB_USER"],
+                                         password=os.environ["DB_PASSWORD"],
+                                         host=os.environ["DB_HOST"],
                                          port="5432",
-                                         db=os.environ['DB_NAME'])
+                                         db=os.environ["DB_NAME"])
 engine = create_engine(URI, pool_pre_ping=True)
 session_local = sessionmaker(bind=engine, autocommit=False, autoflush=False)

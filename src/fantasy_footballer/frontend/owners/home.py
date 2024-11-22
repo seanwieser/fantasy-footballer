57deb038a8b304547f08c870ac6339c60e6ba373
seanwieser <seanwieser@gmail.com>
2024-11-22 14:11:53 -0800

    Hook frontend to new backend.

--- src/fantasy_footballer/frontend/owners/home.py
+++ src/fantasy_footballer/frontend/owners/home.py
@@ -1,72 +1,67 @@
 """Module for Owners page."""
 
 import datetime
+import os
 
-from backend.fetch import START_YEAR
-from backend.io_utils import MEDIA_PATH_TEMPLATE
-from backend.models import Team
-from frontend.utils import (common_header, get_fantasy_years,
-                            image_path_to_owner_name, query_data)
-from inflection import titleize
+from frontend.utils import common_header, get_years, image_path_to_owner_id
 from nicegui import events, ui
-from sqlalchemy import func, select
+
+from fantasy_footballer.backend.db import DbManager
 
 
 def mouse_handler(event: events.MouseEventArguments, year=int):
-    """Mouse event handler for Owners page images."""
-    owner_full_name = image_path_to_owner_name(event.sender.source)
+    """Mouse event handler for Owners page image."""
+    owner_id = image_path_to_owner_id(event.sender.source)
     if event.type == "mouseup":
-        ui.navigate.to(f"/owners/{owner_full_name}/{year}")
-    elif event.type == "mouseover" and owner_full_name == "jack_hayes":
+        ui.navigate.to(f"/owners/{owner_id}/{year}")
+    elif event.type == "mouseover" and str(owner_id) == "4":
         ui.notify("Dick sucks at fantasy football")
 
 
-def owners_grid(img_paths, year):
+def owners_grid(owners_info, year):
     """Grid component for each tab."""
     with ui.grid(rows=2, columns=6).classes("h-100 gap-1"):
-        for img_path in sorted(img_paths):
-            label_text = titleize(image_path_to_owner_name(img_path))
+        for owner_info in owners_info:
             with ui.interactive_image(
-                    img_path,
+                    owner_info["image_path"],
                     on_mouse=lambda e, year=year: mouse_handler(e, year),
                     events=["mousedown", "mouseup", "mouseover"]):
                 label_class = [
                     "absolute-bottom", "text-center", "text-weight-bold",
                     "bg-black", "text-white", "p-0", "m-1", "rounded-lg"
                 ]
-                ui.label(label_text).classes(" ".join(label_class))
+                ui.label(owner_info["owner_name"]).classes(" ".join(label_class))
 
 
-async def get_img_paths_by_year():
+def get_owners_info_by_year():
     """Query database for owners by year and construct image paths."""
-    owner_field_label = "owners"
-    executable = select(Team.year, func.array_agg(Team.owner).label(owner_field_label)).group_by(Team.year) # pylint: disable=not-callable
-    rows = await query_data(executable)
+    owners_df = DbManager.query("select * from main_marts.owners_by_year")
 
-    # Transform query results into dictionary of image paths by year
-    img_paths = {}
-    for row in rows:
-        img_paths[row["year"]] = []
-        for full_name in row[owner_field_label]:
-            image_path = MEDIA_PATH_TEMPLATE.substitute(sub_path="owners", file_name=f"{full_name}.jpg")
-            img_paths[row["year"]].append(image_path)
-    return img_paths
+    # Transform query results into dictionary of owner_id, owner_name by year
+    owners_info_by_year = {}
+    for row in owners_df.to_dict("records"):
+        owners_info = []
+        for owner_info in row["owners"]:
+            owner_info["image_path"] = f"{os.getenv('MEDIA_DIR_PATH')}/owners/{owner_info["owner_id"]}.jpg"
+            owners_info.append(owner_info)
+        owners_info_by_year[row["year"]] = sorted(owners_info, key=lambda d: d["owner_name"])
+    return owners_info_by_year
 
 
 @ui.page("/owners")
-async def page():
+def page():
     """Owners page with tabs for each year."""
     common_header()
-    img_paths_by_year = await get_img_paths_by_year()
+    owners_info_by_year = get_owners_info_by_year()
 
     current_year = datetime.datetime.now().year
+    fantasy_years = get_years()
     with ui.tabs().classes("w-full") as tabs:
         tab_panels = {}
-        fantasy_years = await get_fantasy_years()
         for year in fantasy_years:
             tab_panels[year] = ui.tab(str(year))
 
     with ui.tab_panels(tabs, value=str(current_year)).classes("w-full"):
-        for year in range(START_YEAR, current_year + 1):
+        for year in fantasy_years:
             with ui.tab_panel(tab_panels[year]):
-                owners_grid(img_paths_by_year[year], year)
+                owners_grid(owners_info_by_year[year], year)

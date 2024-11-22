8a5132f09dd59a409a1ee30a28fd458c1c1c5467
seanwieser <seanwieser@gmail.com>
2024-11-22 14:08:59 -0800

    Add dbt project.

---
+++ dbt/fantasy_footballer/models/marts/splash/current_standings.sql
@@ -0,0 +1,12 @@
+select
+    standing                 as "Standing",
+    team_name                as "Name",
+    owner_name               as "Owner",
+    wins                     as "Wins",
+    losses                   as "Losses",
+    round(points_for, 2)     as "Points For",
+    round(points_against, 2) as "Points Against",
+    streak                   as "Streak"
+from {{ ref("stg_s001__teams" )}}
+where year = {{ modules.datetime.datetime.now().year }}
+order by standing

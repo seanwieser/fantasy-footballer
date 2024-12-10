db7687e1c6a2dcfab30f5a4280460088fe0e7f1f
seanwieser <seanwieser@gmail.com>
2024-12-09 20:07:53 -0800

    Add refresh data functionality.

--- src/fantasy_footballer/backend/db.py
+++ src/fantasy_footballer/backend/db.py
@@ -1,37 +1,104 @@
 """Module used by frontend to perform database actions and provide interface for frontend."""
 
+import json
 import os
+from datetime import datetime
+from string import Template
 
+import boto3
 import duckdb
 from backend.sources.s001.extract import S001Extractor
 from dbt.cli.main import dbtRunner, dbtRunnerResult
 
-SOURCE_EXTRACTORS = [S001Extractor()]
+SOURCE_EXTRACTOR_MAP = {e.SOURCE_NAME: e for e in [S001Extractor]}
+
+SECRET_SQL = f"""
+CREATE OR REPLACE SECRET cloud_storage_secret (
+    TYPE S3,
+    KEY_ID '{os.getenv("ACCESS_KEY")}',
+    SECRET '{os.getenv("SECRET_KEY")}',
+    ENDPOINT '{os.getenv("ENDPOINT").replace("https://", "")}',
+    REGION '{os.getenv("REGION")}'
+);
+"""
+
+CREATE_SCHEMA_TEMPLATE = Template("CREATE SCHEMA IF NOT EXISTS ${db_name}.${source_name}")
+CREATE_TABLE_TEMPLATE = Template("CREATE OR REPLACE TABLE ${fqtn} AS SELECT * FROM read_json(${file_paths});")
 
 class DbManager:
-    """Static class containing functions to setup database, run dbt, and provide interface to frontend."""
+    """Static class to provide interface to frontend for all data actions (fetch, load, transform, query)."""
 
     @staticmethod
     def setup():
-        """Function to be run at app startup to make polished data available to be queried."""
-        for extractor in SOURCE_EXTRACTORS:
-            DbManager.create_source_tables(extractor)
-        DbManager.run_dbt()
+        """Boot function for backend to load/transform all sources to make db ready for frontend."""
+        DbManager.refresh_db(SOURCE_EXTRACTOR_MAP.keys())
 
     @staticmethod
-    def create_source_tables(source_extractor, refresh=False):
-        """Function to ensure source schemas/tables/data exists for dbt to source from."""
+    def refresh_db(sources, queue=None):
+        """Function used to load fresh data from cloud storage into db and transform with dbt."""
+        # Load data from cloud storage
         db_name, db_path = os.getenv("DB_NAME"), os.getenv("DB_PATH")
+        queue_count = 0
         with duckdb.connect(db_path) as conn:
-            conn.sql(f"CREATE SCHEMA IF NOT EXISTS {db_name}.{source_extractor.SOURCE_NAME}")
+            conn.sql(SECRET_SQL)
+            for source in sources:
+                conn.sql(CREATE_SCHEMA_TEMPLATE.substitute(db_name=db_name, source_name=source))
+                table_paths = DbManager.get_fresh_table_paths(source)
+                for table in SOURCE_EXTRACTOR_MAP[source].get_table_names():
+                    fqtn = f"{db_name}.{source}.{table}"
+                    file_paths_str = json.dumps(table_paths[table])
+                    conn.sql(CREATE_TABLE_TEMPLATE.substitute(fqtn=fqtn, file_paths=file_paths_str))
 
-            for table in source_extractor.tables:
-                source_dir = f"{os.getenv('SOURCE_DIR_PATH')}/{source_extractor.SOURCE_NAME}/{table}"
-                fqtn = f"{db_name}.{source_extractor.SOURCE_NAME}.{table}"
-                sql = f"CREATE TABLE IF NOT EXISTS {fqtn} AS SELECT * FROM read_json('{source_dir}/*.jsonl')"
-                if refresh:
-                    sql = sql.replace("CREATE TABLE IF NOT EXISTS", "CREATE OR REPLACE TABLE")
-                conn.sql(sql)
+                if queue:
+                    queue_count += 1
+                    queue.put_nowait(queue_count / (len(sources) + 1))
+
+        # Transform
+        DbManager.run_dbt()
+        if queue:
+            queue_count += 1
+            queue.put_nowait(queue_count / (len(sources) + 1))
+
+    @staticmethod
+    def fetch_data(years, source, tables, queue):
+        """Run source extractor for passed parameters."""
+        SOURCE_EXTRACTOR_MAP[source].run(queue, years, tables)
+
+    @staticmethod
+    def get_all_tables_by_source():
+        """Utility to have all table names associated with each source."""
+        return {source_name: ex.get_table_names() for source_name, ex in SOURCE_EXTRACTOR_MAP.items()}
+
+    @staticmethod
+    def get_fresh_table_paths(source):
+        """Fetch cloud storage paths for freshest data for each table/year combination for passed source."""
+        s3_client = boto3.client("s3",
+                                 endpoint_url=os.getenv("ENDPOINT"),
+                                 aws_access_key_id=os.getenv("ACCESS_KEY"),
+                                 aws_secret_access_key=os.getenv("SECRET_KEY"))
+        objects = s3_client.list_objects_v2(Bucket=os.getenv("BUCKET_NAME"))
+
+        # Gather all the freshest date string for each table/year combination
+        newest_dates={t: {} for t in DbManager.get_all_tables_by_source()[source]}
+        for file_info in objects["Contents"]:
+            file_name = file_info["Key"].split("/")[-1]
+            if file_name.startswith(source):
+                _, file_table, file_year, file_date = file_name.replace(".json", "").split("_")
+                if not newest_dates[file_table].get(file_year):
+                    newest_dates[file_table][file_year] = file_date
+                elif datetime.fromisoformat(newest_dates[file_table][file_year]) < datetime.fromisoformat(file_date):
+                    newest_dates[file_table][file_year] = file_date
+
+        # Compile all file paths for freshest data using information gathered above
+        fresh_table_paths = {table: [] for table in newest_dates}
+        for table, dates_by_year in newest_dates.items():
+            for year, date in dates_by_year.items():
+                dir_path = f"{os.getenv('SOURCE_DIR_PATH')}/{source}/{table}/{year}/{date}"
+                file_name = f"{source}_{table}_{year}_{date}.json"
+                fresh_table_paths[table].append(f"s3://{os.getenv('BUCKET_NAME')}/{dir_path}/{file_name}")
+
+        return fresh_table_paths
+
 
     @staticmethod
     def run_dbt():
--- src/fantasy_footballer/frontend/admin/home.py
+++ src/fantasy_footballer/frontend/admin/home.py
@@ -1,20 +1,20 @@
-import os
+"""Module for Admin page."""
+
+from multiprocessing import Manager
 
 from backend.db import DbManager
 from frontend.utils import common_header, get_valid_years
-from nicegui import ui, app, run
-import datetime
-from functools import partial
-from multiprocessing import Manager, Queue
-
-BACKUP_EXTENSIONS = ["jsonl", "ico", "duckdb", "jpg"]
+from nicegui import app, run, ui
 
 
 @ui.page("/admin")
 async def page():
     """Admin page to manually execute backend actions from the frontend."""
-    async def fetch_data_w_progress_bar(years, source_tables):
+    def update_progressbars():
+        fetch_progressbar.set_value(fetch_queue.get() if not fetch_queue.empty() else fetch_progressbar.value)
+        refresh_progressbar.set_value(refresh_queue.get() if not refresh_queue.empty() else refresh_progressbar.value)
 
+    async def async_fetch_data(years, source_tables):
         # Need a better way to repr the source tables mapping
         tables_by_source = {}
         for source_table in source_tables:
@@ -25,26 +25,35 @@ async def page():
                 tables_by_source[source] = [table]
 
         for source, tables in tables_by_source.items():
-            await run.cpu_bound(DbManager.fetch_data, years, source, tables, queue)
+            await run.cpu_bound(DbManager.fetch_data, years, source, tables, fetch_queue)
+
+    async def async_refresh_data(sources):
+        await run.cpu_bound(DbManager.refresh_db, sources, refresh_queue)
 
     common_header()
 
-    queue = Manager().Queue()
-    ui.timer(1, callback=lambda: progressbar.set_value(queue.get() if not queue.empty() else progressbar.value))
+    ui.timer(1, callback=update_progressbars)
 
-    # Need a better way to repr the source tables mapping
+    source_info = DbManager.get_all_tables_by_source()
+
+    # Fetch components
     options = []
-    for source_name, table_names in DbManager.get_all_tables_by_source().items():
+    for source_name, table_names in source_info.items():
         for table_name in table_names:
-            options.append(f"{source_name}: {table_name}")
-
+            options.append(f"{source_name}: {table_name}") # Need a better way to repr the source tables mapping
     with ui.row():
         source_table_selections = ui.select(options, value=None, multiple=True, clearable=True)
         year_selections = ui.select(get_valid_years(), value=None, multiple=True, clearable=True)
-
-
-    ui.button("Fetch", on_click=lambda: fetch_data_w_progress_bar(year_selections.value, source_table_selections.value))
-    progressbar = ui.linear_progress(value=0).props("instant-feedback")
-
+    fetch_queue = Manager().Queue()
+    ui.button("Fetch", on_click=lambda: async_fetch_data(year_selections.value, source_table_selections.value))
+    fetch_progressbar = ui.linear_progress(value=0).props("instant-feedback")
     ui.separator()
+
+    # Refresh components
+    refresh_queue = Manager().Queue()
+    source_selections = ui.select(list(source_info.keys()), value=None, multiple=True, clearable=True)
+    ui.button("Refresh Data", on_click=lambda: async_refresh_data(source_selections))
+    refresh_progressbar = ui.linear_progress(value=0).props("instant-feedback")
+    ui.separator()
+
     ui.button("Shutdown", on_click=app.shutdown)

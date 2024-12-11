8c87e0aa0acce247b9e8f4c2c1df20e686e58877
seanwieser <seanwieser@gmail.com>
2024-12-11 12:39:11 -0800

    Remove start year as an env var

--- src/fantasy_footballer/frontend/utils.py
+++ src/fantasy_footballer/frontend/utils.py
@@ -7,11 +7,12 @@ from inflection import humanize
 from nicegui import app, context, elements, ui
 from pandas import DataFrame
 
+START_YEAR = 2018
 PAGES = ["owners", "admin"]
 
 def get_valid_years() -> list[int]:
     """Get all years that fantasy data is available for."""
-    return list(range(int(os.getenv('START_YEAR')), datetime.datetime.now().year + 1))
+    return list(range(START_YEAR, datetime.datetime.now().year + 1))
 
 def get_years() -> list[str]:
     """Get all years that have fantasy data."""

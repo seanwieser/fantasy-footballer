c4d1acf73013b722ad37a7f7f81a5e140783d069
seanwieser <seanwieser@gmail.com>
2025-02-07 16:03:50 -0800

    video embedding example

---
+++ src/fantasy_footballer/frontend/gallery/home.py
@@ -0,0 +1,8 @@
+from nicegui import ui
+
+
+@ui.page("/gallery")
+def page():  # pylint:disable=too-many-statements
+    with ui.card_section().classes("mx-auto").classes("p-0 w-full h-full"):
+        ui.html(
+            f"""<div style="position:relative;padding-top:56.25%;"><iframe src="https://iframe.mediadelivery.net/embed/366287/9ad736fe-b905-43fa-a4d7-1b8cf5e4facd?autoplay=false&loop=false&muted=false&preload=false&responsive=true" loading="lazy" style="border:0;position:absolute;top:0;height:100%;width:100%;" allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;" allowfullscreen="true"></iframe></div>""")

f761ae23ed127bb4b89804c6bb9b9bfde4474c7c
seanwieser <seanwieser@gmail.com>
2025-10-11 14:47:37 -0700

    precommit

--- src/fantasy_footballer/frontend/gallery/home.py
+++ src/fantasy_footballer/frontend/gallery/home.py
@@ -1,3 +1,4 @@
+"""Module for Gallery page for users to view league related media."""
 # pylint:disable=too-many-statements, disable=line-too-long
 from frontend.utils import common_header
 from nicegui import ui
@@ -5,17 +6,19 @@ from nicegui import ui
 
 @ui.page("/gallery")
 def page():
+    """Gallery Page to display media hosted by bunny cdn."""
     common_header()
-    with ui.card_section().classes("mx-auto").classes("p-0 w-full h-full"):
-        ui.html(
-            r"""
-        <div style="position:relative;padding-top:56.25%;">
-        <iframe 
-            src="https://iframe.mediadelivery.net/embed/366287/9ad736fe-b905-43fa-a4d7-1b8cf5e4facd?autoplay=false&loop=false&muted=false&preload=false&responsive=true" 
-            loading="lazy" 
-            style="border:0;position:absolute;top:0;height:100%;width:100%;" 
-            allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;" 
-            allowfullscreen="true"
-        >
-        </iframe>
-        </div>""")
+    ui.label("Under Construction...")
+    # with ui.card_section().classes("mx-auto").classes("p-0 w-full h-full"):
+    #     ui.html(
+    #         r"""
+    #     <div style="position:relative;padding-top:56.25%;">
+    #     <iframe
+    #         src="https://iframe.mediadelivery.net/embed/366287/9ad736fe-b905-43fa-a4d7-1b8cf5e4facd?autoplay=false&loop=false&muted=false&preload=false&responsive=true"
+    #         loading="lazy"
+    #         style="border:0;position:absolute;top:0;height:100%;width:100%;"
+    #         allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;"
+    #         allowfullscreen="true"
+    #     >
+    #     </iframe>
+    #     </div>""")

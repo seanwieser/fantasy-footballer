630b8e7487459335c3e499d0572c0f700356eccc
seanwieser <wfbvphvg6m@privaterelay.appleid.com>
2025-10-11 17:22:27 -0700

    Shotgun bug; Development Optimization (#2)
    * add stats center page
    
    * remove stats center stuff
    
    * remove dbt seeds
    
    * remove dbt seeds
    
    * revert dev commenting
    
    * remove stats center stuff
    
    * formatting
    
    * add dbt console print
    
    * fix admin page; fix admin user privileges
    
    ---------
    
    Co-authored-by: seanwieser <seanwieser@gmail.com>

--- dbt/fantasy_footballer/models/intermediate/shotguns/int_shotguns.sql
+++ dbt/fantasy_footballer/models/intermediate/shotguns/int_shotguns.sql
@@ -1,5 +1,6 @@
 with played_weeks_w_lowest as (
     select
+        owner_id,
         team_id,
         team_schedule_id,
         year,
@@ -7,17 +8,18 @@ with played_weeks_w_lowest as (
         score_for,
         min(score_for) over (partition by year, week) as week_min_score_for
     from {{ ref('stg_s001__team_schedules') }}
-    where outcome != 'U'
+    where outcome != 'U' and not is_playoff
 )
 
 select
+    played_weeks_w_lowest.owner_id,
     played_weeks_w_lowest.team_id,
     played_weeks_w_lowest.team_schedule_id,
-    teams.owner_name,
+    owner_names.owner_name,
     played_weeks_w_lowest.year,
     played_weeks_w_lowest.week,
     played_weeks_w_lowest.score_for < 100 or
     played_weeks_w_lowest.score_for = played_weeks_w_lowest.week_min_score_for as is_shotgun
 from played_weeks_w_lowest
-inner join {{ ref('stg_s001__teams') }} as teams
-    on played_weeks_w_lowest.team_id = teams.team_id
+inner join {{ ref("owner_names") }} as owner_names
+    on played_weeks_w_lowest.owner_id = owner_names.owner_id
